22c49b146d758d822c2dc442cafb82d678db24ff
Baisheng <baisheng@gmail.com>
2019-08-20 19:32:25 +0800

    rebase

--- src/helper/wrapPageElement.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import TransitionWrapper from "./transitionWrapper"
-
-const wrapPageElement = ({ element, props }) => {
-  return <TransitionWrapper {...props} >{element}</TransitionWrapper>
-}
-
-export default wrapPageElement
